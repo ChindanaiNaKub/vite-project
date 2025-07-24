9bb92f5791db92e152ae9f267f6b7c5321099d6b
ChindanaiNaKub <chindanai_jaiman@cmu.ac.th>
2025-07-25 02:30:31 +0700

    4.2

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,13 +1,21 @@
 <script lang="ts" setup>
 import { toRefs } from 'vue';
 import  { type Event } from '@/types'
+import { useRouter } from 'vue-router'
 const props = defineProps<{
     event: Event
     id: string
 }>()
 const { event } = toRefs(props)
+const router = useRouter()
+const register = () => {
+    // if regis api call successful
+    // push back to the event details view
+    router.push({ name: 'event-detail-view'})
+}
 </script>
 
 <template>
  <p>Register event here</p>
+ <button @click="register">Register</button>
 </template>
