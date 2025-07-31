c6c0b72b4f22a73d6d029173b8e0fc5b7b9ad291
ChindanaiNaKub <chindanai_jaiman@cmu.ac.th>
2025-07-31 20:49:33 +0700

    9.1

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,9 @@ const { message } = storeToRefs(store)
         <h4>{{ message }}</h4>
       </div>
       <div class="wrapper">
-        <nav>
-          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink>
-          <span class="nav-separator">|</span>
-          <RouterLink :to="{ name: 'about' }">About</RouterLink>
-          <span class="nav-separator">|</span>
-          <RouterLink :to="{ name: 'Students' }">Student</RouterLink>
+        <nav class="py-6">
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'event-list-view' }">Event</RouterLink> |
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'about' }">About</RouterLink>
         </nav>
       </div>
     </header>
